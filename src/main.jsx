44caa500f047af45070ee60cb490c8d78ddd1811
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-10 17:46:41 -0300

    Reseta componente home

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import theme from './styles/theme'
-import { Header } from './components/Header'
+import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header/>
+      <Home/>
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,11 @@
-import { Container, ContentHeader } from './styles'
+import { Container } from './styles'
 import { Header } from '../../components/Header'
-import { Button } from '../../components/Button'
-import { FiPlus } from 'react-icons/fi'
 
 export function Home() {
   return(
     <>
       <Header />
       <Container>
-        <ContentHeader>
-          <h1>Meus filmes</h1>
-          <Button title="Adicionar filme" icon={FiPlus} />
-        </ContentHeader>
       </Container>
     </>
   )
