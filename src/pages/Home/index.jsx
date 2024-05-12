3cf7986b1c5ab832e6b874a846369effdeafdf0e
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-12 12:07:27 -0300

    Finaliza página home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,59 @@
-import { Container } from './styles'
+import { Container, MoviesList, MainHeader, MovieItem, TagsList } from './styles'
 import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { FiPlus } from 'react-icons/fi'
 
 export function Home() {
   return(
     <>
       <Header />
       <Container>
+        <MainHeader>
+          <h2>Meus filmes</h2>
+          <Button title="Adicionar" icon={FiPlus}/>
+        </MainHeader>
+        <main>
+          <MoviesList>
+            <MovieItem>
+              <h3>Interestellar</h3>
+              <p>
+                Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária
+                em futuro de data desconhecida.
+              </p>
+              <TagsList>
+                <span>ficção científica</span>
+                <span>aventura</span>
+                <span>espaço</span>
+              </TagsList>
+            </MovieItem>
+
+            <MovieItem>
+              <h3>Interestellar</h3>
+              <p>
+                Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária
+                em futuro de data desconhecida.
+              </p>
+              <TagsList>
+                <span>ficção científica</span>
+                <span>aventura</span>
+                <span>espaço</span>
+              </TagsList>
+            </MovieItem>
+
+            <MovieItem>
+              <h3>Interestellar</h3>
+              <p>
+                Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária
+                em futuro de data desconhecida.
+              </p>
+              <TagsList>
+                <span>ficção científica</span>
+                <span>aventura</span>
+                <span>espaço</span>
+              </TagsList>
+            </MovieItem>
+          </MoviesList>
+        </main>
       </Container>
     </>
   )
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,20 +2,76 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   padding: 5rem 12.3rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 4.5rem;
+
+  & > main {
+    overflow-y: scroll;
+  }
 `
 
-export const ContentHeader = styled.div`
+export const MainHeader = styled.div`
   display: flex;
+  flex-direction: row;
   align-items: center;
   justify-content: space-between;
 
-  h1 {
+  & > h2 {
     font-size: 3.2rem;
     font-weight: 400;
     color: ${({ theme }) => theme.COLORS.WHITE};
   }
 
-  button {
+  & > button {
     width: 20rem;
   }
 `
+
+export const MoviesList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  height: 45rem;
+`
+
+export const MovieItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  width: 100%;
+
+  background-color: ${({ theme }) => theme.COLORS.PINK_OPACITY};
+  border: none;
+  border-radius: 10px;
+  padding: 3.2rem;
+
+  > h3 {
+    font-size: 2.4rem;
+    font-weight: 700;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  > p {
+    font-size: 1.6rem;
+    font-family: 'Roboto' sans-serif;
+    color: ${({ theme }) => theme.COLORS.GRAY_150};
+    text-align: justify;
+  }
+`
+
+export const TagsList = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: .8rem;
+
+  span {
+    background-color: ${({ theme }) => theme.COLORS.GRAY_300};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    border-radius: 8px;
+    padding: 5px 1.6rem;
+  }
+`
+
