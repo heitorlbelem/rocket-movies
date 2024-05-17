75f39f96134a419c774adf667017a749c5643fb7
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-17 20:24:18 -0300

    Refatora página de SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,22 +1,29 @@
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { TextButton } from "../../components/TextButton";
-import { Container, Background, Form } from "./styles";
+import { Container, Background, Form, FormTitle, InputsContainer, ButtonsContainer } from "./styles";
 import { FiMail, FiLock } from "react-icons/fi";
 
 export function SignIn() {
   return(
     <Container>
       <Form>
-        <h1>RocketMovies</h1>
-        <p>Aplicação para acompanhar tudo que assistir.</p>
+        <FormTitle>
+          <h1>RocketMovies</h1>
+          <p>Aplicação para acompanhar tudo que assistir.</p>
+        </FormTitle>
+
         <h2>Faça seu login</h2>
 
-        <Input type="text" placeholder="E-mail" icon={FiMail} />
-        <Input type="password" placeholder="Senha" icon={FiLock} />
+        <InputsContainer>
+          <Input type="text" placeholder="E-mail" icon={FiMail} />
+          <Input type="password" placeholder="Senha" icon={FiLock} />
+        </InputsContainer>
 
-        <Button title="Entrar" />
-        <TextButton title="Criar conta" />
+        <ButtonsContainer>
+          <Button title="Entrar" />
+          <TextButton title="Criar conta" />
+        </ButtonsContainer>
       </Form>
       <Background />
     </Container>
