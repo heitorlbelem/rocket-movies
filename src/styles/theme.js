7b4675d6af92230fa91f3f4b029e0743481c4fa6
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-17 20:24:33 -0300

    Refatora constante de cores da aplicação

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,16 +1,13 @@
 export default {
   COLORS: {
     GRAY_100: "#CAC4CF",
-    GRAY_150: "#999591",
-    GRAY_200: "#3E3B47",
-    GRAY_300: "#312E38",
+    GRAY_200: "#999591",
+    GRAY_300: "#3E3B47",
+    GRAY_400: "#312E38",
     GRAY_700: "#262529",
     GRAY_900: "#1C1B1E",    
-    
-    PINK_OPACITY: "rgba(255, 133, 155, 0.1)",
+    BLACK: '#0D0C0F',
     PINK: "rgba(255, 133, 155, 1)",
-    PINK_LIGHT: "rgba(255, 140, 155, 1)",
-    WHITE: '#FFF',
-    BLACK: '#0D0C0F'
+    WHITE: '#F4EDE8',
   }
 }
